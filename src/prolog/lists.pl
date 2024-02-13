d9f614735f492640e34e85bfd0449343ea897a70
emi <hr.emii87@gmail.com>
2024-02-12 21:09:30 -0800

    Feature: Minor changes

--- src/prolog/lists.pl
+++ src/prolog/lists.pl
@@ -13,5 +13,23 @@ concat([X|R], L, [X|Q]):- concat(R, L, Q).
 double([], []).
 double([X|R], [X, X|Q]):- double(R, Q).
 
+% It's like a tree, it goes into the reverse relation
+% recursively (going until the leaf of the branch) and
+% then goes into the concat relation parallel to each
+% call starting to concat the last element with an empty
+% list and forwarding it to the previous node. Then that
+% node concats the incoming list with the X value that was
+% separated. The concat calls also are kinda recursive.
+
+% Reverse relation branch    /*\
+%                           /   \ Concat relation branch
+%                          *     *
+%                         / \    ...
+%                        ... ...
+
+
 reverse([], []).
 reverse([X|R], L) :- reverse(R, Q), concat(Q, [X], L).
+
+sum([], 0).
+sum([X|R], S) :- sum(R, SUM), S is SUM + X.
